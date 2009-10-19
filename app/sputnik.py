b49c6a2295c2343f0bf91a0b18b3d405fea09899
christian.plesner.hansen <christian.plesner.hansen@0941ce9a-398d-11de-82fb-7f665ef830f7>
2009-10-19 16:53:40 +0000

    Added browser icons and placement logic in client code

--- app/sputnik.py
+++ app/sputnik.py
@@ -5,6 +5,7 @@
 
 import models
 import os.path
+import math
 import random
 import re
 from google.appengine.ext import webapp
@@ -49,11 +50,182 @@ class RandomCache(object):
       self._vector[index] = value
 
 
+class Plotter(object):
+
+  def __init__(self, app, names, scores, distances):
+    self.app = app
+    self.names = names
+    self.scores = scores
+    self.distances = distances
+
+  def distance(self, a, b):
+    if a == b:
+      return 0
+    elif a == -1:
+      return self.scores[b]
+    elif b == -1:
+      return self.scores[a]
+    elif a < b:
+      return self.distances[b-1][a]
+    else:
+      return self.distances[a-1][b]
+
+  def position(self):
+    clusters = []
+    for i in xrange(len(self.names)):
+      clusters.append(Cluster(self, [i], []))
+    iters = 0
+    # Calculate a hierarchical clustering for the points.  Note that
+    # this algorithm runs in O(n^3) (at least) so if we want to use
+    # this for a large number of points it should be improved.  It can
+    # definitely be made O(n^2).
+    while len(clusters) > 1:
+      iters += 1
+      if iters == 20:
+        break
+      min_dist = None
+      min_i = None
+      min_j = None
+      # Find the two clusters that are closest to each other.
+      for i in xrange(len(clusters)):
+        for j in xrange(i):
+          if min_dist is None:
+            min_dist = clusters[i].distance(clusters[j])
+            min_i = i
+            min_j = j
+            continue
+          a = clusters[i]
+          b = clusters[j]
+          dist = a.distance(b)
+          if dist < min_dist:
+            min_dist = dist
+            min_i = i
+            min_j = j
+      # Remove the two clusters and add their combination
+      left = clusters[min_i]
+      right = clusters[min_j]
+      next_values = left.values + right.values
+      next = Cluster(self, next_values, [left, right])
+      indices = [k for k in xrange(len(clusters)) if k != min_i and k != min_j]
+      clusters = [clusters[k] for k in indices]
+      clusters.append(next)
+    root = clusters[0]
+    points = self.place(root, 0.0, 0.0, 100.0, 100.0, True)
+    cx = 50.0
+    cy = 50.0
+    center = Point(self, cx, cy, -1)
+    points.append(center)
+    self.adjust(points)
+    points = points[:-1]
+    for point in points:
+      point.x -= (center.x - cx)
+      point.y -= (center.y - cy)
+    return points
+
+  def place(self, cluster, min_x, min_y, max_x, max_y, by_x):
+    if not cluster.children:
+      # If this is a leaf we place it right in the middle of the
+      # available area.
+      x_midpoint = (max_x - min_x) / 2
+      y_midpoint = (max_y - min_y) / 2
+      p = Point(self, min_x + x_midpoint, min_y + y_midpoint, cluster.values[0])
+      return [p]
+    left_weight = cluster.children[0].weight()
+    right_weight = cluster.children[1].weight()
+    total_weight = left_weight + right_weight
+    ratio = left_weight / total_weight
+    if by_x:
+      x_midpoint = (max_x - min_x) * ratio
+      one = self.place(cluster.children[0], min_x, min_y, min_x + x_midpoint, max_y, False)
+      two = self.place(cluster.children[1], min_x + x_midpoint, min_y, max_x, max_y, False)
+    else:
+      y_midpoint = (max_y - min_y) * ratio
+      one = self.place(cluster.children[0], min_x, min_y, max_x, min_y + y_midpoint, True)
+      two = self.place(cluster.children[1], min_x, min_y + y_midpoint, max_x, max_y, True)
+    return one + two
+
+  def dampen(self, pull, temp):
+    dx = pull[0]
+    dy = pull[1]
+    length = math.sqrt(dx * dx + dy * dy)
+    if length > temp:
+      ratio = temp / length
+    else:
+      ratio = 1.0
+    pull[0] *= ratio
+    pull[1] *= ratio
+
+  def adjust(self, points):
+    iterations = 60
+    for l in xrange(iterations):
+      temperature = l / float(iterations)
+      pulls = []
+      for i in xrange(len(points)):
+        pulls.append([])
+        for j in xrange(i):
+          pull = points[i].pull(points[j])
+          self.dampen(pull, temperature)
+          pulls[i].append(pull)
+      for i in xrange(len(points)):
+        for j in xrange(i):
+          pull = pulls[i][j]
+          points[i].x += pull[0]
+          points[i].y += pull[1]
+          points[j].x -= pull[0]
+          points[j].y -= pull[1]
+
+
+class Cluster(object):
+
+  def __init__(self, plotter, values, children):
+    self.plotter = plotter
+    self.values = values
+    self.children = children
+
+  def distance(self, other):
+    sum = 0.0
+    count = 0.0
+    for a in self.values:
+      for b in other.values:
+        sum += self.plotter.distance(a, b)
+        count += 1
+    return sum / count
+
+  def weight(self):
+    return float(len(self.values))
+
+  def __str__(self):
+    return str(self.values)
+
+
+class Point(object):
+
+  def __init__(self, app, x, y, type):
+    self.app = app
+    self.x = x
+    self.y = y
+    self.type = type
+    self.icon()
+
+  def pull(self, other):
+    ideal = self.plotter.distance(self.id, other.id) / 9
+    dx = self.x - other.x
+    dy = self.y - other.y
+    if ideal == 0:
+      return [-dx, -dy]
+    dist = (ideal - math.sqrt(dx * dx + dy * dy)) / ideal
+    return [dx * dist, dy * dist]
+
+  def icon(self):
+    return self.app.get_icon(self.type)
+
+
 class Sputnik(object):
 
   def __init__(self):
     self._case_source_cache = RandomCache(_CASE_CACHE_SIZE)
     self._plot_cache = RandomCache(_PLOT_CACHE_SIZE)
+    self._icon_cache = { }
 
   def do_404(self, req):
     req.error(404)
@@ -62,8 +234,11 @@ class Sputnik(object):
     req.response.out.write(message)
     req.error(500)
 
+  def get_dynamic_path(self, name):
+    return os.path.join(os.path.dirname(__file__), 'dynamic', name)
+
   def get_template(self, name, attribs):
-    path = os.path.join(os.path.dirname(__file__), 'dynamic', name)
+    path = self.get_dynamic_path(name)
     return template.render(path, attribs)
 
   def get_main_page(self, req):
@@ -118,32 +293,37 @@ class Sputnik(object):
       return self.do_404(req)
     req.response.out.write(suite.to_json())
 
-  def expand_matrix(self, m):
-    raw_matrix = [s.split(',') for s in m.split(':')]
-    headers = raw_matrix[0]
-    rows = [[int(i) for i in r] for r in raw_matrix[1:]]
-    points = []
-    for x in xrange(5, 100, 10):
-      for y in xrange(5, 100, 10):
-        points.append({'x': x, 'y': y})
-    return points
+  def get_icon(self, type):
+    path = self.get_dynamic_path('%s.svg' % type)
+    if os.path.exists(path):
+      f = open(path)
+      c = f.read()
+      f.close()
+      c = re.sub(r"^(.|[\n])*<!--\s+begin\s+icon\s+-->", "", c, re.MULTILINE)
+      c = re.sub(r"<!--\s+end\s+icon\s+-->(.|[\n])*$", "", c, re.MULTILINE)
+      return c
+    else:
+      return ''
 
   def get_comparison_plot(self, req):
     req.response.headers['Content-Type'] = 'image/svg+xml'
-    matrix_param = req.request.params.get('m', None)
-    if matrix_param:
-      key = matrix_param
-      matrix = self.expand_matrix(matrix_param)
-      draw_points = True
+    points = req.request.params.get('m', None)
+    if points:
+      point_objs = []
+      for p in points.split(':'):
+        [type, point] = p.split('@')
+        [x, y] = point.split(',')
+        point_objs.append(Point(self, x, y, type))
+      key = points
+      plot = self.get_template('plot.svg', {
+        'draw_points': True,
+        'points': point_objs
+      })
     else:
       key = 'none'
-      matrix = ''
-      draw_points = False
-    #if not key in self._plot_cache:
-    plot = self.get_template('plot.svg', {
-      'draw_points': draw_points,
-      'points': matrix
-    })
+      plot = self.get_template('plot.svg', {
+        'draw_points': False,
+      })
     self._plot_cache[key] = plot
     req.response.out.write(self._plot_cache[key])
 
