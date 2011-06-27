9baa9323236b80d144b380062eaf28d20a77d8cf
erights <erights@0941ce9a-398d-11de-82fb-7f665ef830f7>
2011-06-27 14:33:14 +0000

    More tests for bad this values. Fixed tests for nested function declarations to only test strict code. Review URL: http://codereview.appspot.com/4172048

--- trunk/tests/Conformance/15_Native_ECMA_Script_Objects/15.2_Object_Objects/15.2.4_Properties_of_the_Object_Prototype_Object/15.2.4.4_Object.prototype.valueOf/S15.2.4.4_A14.js
+++ trunk/tests/Conformance/15_Native_ECMA_Script_Objects/15.2_Object_Objects/15.2.4_Properties_of_the_Object_Prototype_Object/15.2.4.4_Object.prototype.valueOf/S15.2.4.4_A14.js
@@ -2,11 +2,11 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /**
-* @name: S15.2.4.4_A13;
+* @name: S15.2.4.4_A14;
 * @section: 15.2.4.4;
 * @assertion: Let O be the result of calling ToObject passing the this
 *             value as the argument.
 * @negative
 */
 
-Object.prototype.valueOf.call(null);
+(1,Object.prototype.valueOf)();
